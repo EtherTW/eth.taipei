590de42c667269782056b73210779c84fc99c119
Chih Cheng Liang <chihchengliang@gmail.com>
2019-08-03 00:34:54 +0800

    Updates

---
+++ precache-manifest.25b19bd9497ab2ef90ae0d00810f51de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57cd431a26c34f7e954931f81a49851",
+    "url": "/eth.taipei/index.html"
+  },
+  {
+    "revision": "75b73ec56cf9d82af084",
+    "url": "/eth.taipei/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e37450bb98cb92e003c1",
+    "url": "/eth.taipei/static/css/main.02e31efa.chunk.css"
+  },
+  {
+    "revision": "75b73ec56cf9d82af084",
+    "url": "/eth.taipei/static/js/2.a2465d79.chunk.js"
+  },
+  {
+    "revision": "e37450bb98cb92e003c1",
+    "url": "/eth.taipei/static/js/main.72afabd2.chunk.js"
+  },
+  {
+    "revision": "65aba234cbb94e4df9f9",
+    "url": "/eth.taipei/static/js/runtime~main.08b3d580.js"
+  },
+  {
+    "revision": "c67f6de1b8afc128bf2c00195650189a",
+    "url": "/eth.taipei/static/media/background.c67f6de1.png"
+  },
+  {
+    "revision": "c50f665ea4a160608584833cbcc03964",
+    "url": "/eth.taipei/static/media/logo.c50f665e.png"
+  }
+]);
